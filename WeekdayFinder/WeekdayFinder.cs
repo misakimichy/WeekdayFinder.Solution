36dcbe7c7895a4f9eb9c178126c93bf03273dfc9
Misaki Koonce and Nina Potrebich <nina@>
2019-12-18 11:50:49 -0800

    Add UI logic.

--- WeekdayFinder/WeekdayFinder.cs
+++ WeekdayFinder/WeekdayFinder.cs
@@ -1,4 +1,5 @@
 using System;
+using WeekdayFinder.Models;
 
 namespace WeekdayFinder
 {
@@ -6,7 +7,15 @@ namespace WeekdayFinder
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Welcome to the date Weekday Finder");
+            Console.WriteLine("Please input a day of the year and we'll return the date.");
+            Console.WriteLine("Which day (dd)");
+            int dayInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Which month (mm)");
+            int monthInput = int.Parse(Console.ReadLine());
+            Console.WriteLine("Which year (yyyy)");
+            int yearInput = int.Parse(Console.ReadLine());
+            Console.WriteLine($"It's {Date.GetDate(dayInput, monthInput, yearInput)}.");
         }
     }
 }
