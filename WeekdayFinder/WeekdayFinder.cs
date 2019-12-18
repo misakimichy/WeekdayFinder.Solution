975ef1f751f0dc4ecee7a0b87ac4a1496da6954b
Misaki Koonce and Nina Potrebich <misaki@>
2019-12-18 13:14:40 -0800

    change main()

--- WeekdayFinder/WeekdayFinder.cs
+++ WeekdayFinder/WeekdayFinder.cs
@@ -9,11 +9,11 @@ namespace WeekdayFinder
         {
             Console.WriteLine("Welcome to the date Weekday Finder");
             Console.WriteLine("Please input a day of the year and we'll return the date.");
-            Console.WriteLine("Which day (dd)");
+            Console.Write("Which day (dd):");
             int dayInput = int.Parse(Console.ReadLine());
-            Console.WriteLine("Which month (mm)");
+            Console.Write("Which month (mm):");
             int monthInput = int.Parse(Console.ReadLine());
-            Console.WriteLine("Which year (yyyy)");
+            Console.Write("Which year (yyyy):");
             int yearInput = int.Parse(Console.ReadLine());
             Console.WriteLine($"It's {Date.GetDate(dayInput, monthInput, yearInput)}.");
         }
